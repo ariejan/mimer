3379b6dffe8a72a6a4628e1689e24a0e9567820d
Ariejan de Vroom <ariejan@ariejan.net>
2015-02-17 11:44:43 +0100

    Refactor specs to use expect-syntax

--- spec/mimer_spec.rb
+++ spec/mimer_spec.rb
@@ -1,64 +1,64 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Mimer" do
-    
+
   it "should create a new Mimer instance with identify" do
     m = Mimer.identify(fixture_path('plain.txt'))
-    
-    m.should_not be_nil
-    m.should be_a(Mimer)
+
+    expect(m).not_to be_nil
+    expect(m).to be_a(Mimer)
   end
-  
+
   it "should return nil if the file cannot be found" do
     m = Mimer.identify(fixture_path('does_not.exist'))
-    
-    m.should be_nil
+
+    expect(m).to be_nil
   end
-  
+
   describe "text" do
-    
+
     before(:each) do
       @mimer = Mimer.identify(fixture_path('plain.txt'))
     end
-    
+
     it "should identify mime correctly" do
-      @mimer.mime_type.should match(/text\/plain/)
+      expect(@mimer.mime_type).to match(/text\/plain/)
     end
-    
+
     it "should find this text" do
-      @mimer.should be_text
+      expect(@mimer).to be_text
     end
   end
-  
+
   describe "image extensions" do
     it "should work for jpeg" do
       mimer = Mimer.identify(fixture_path('facepalm.jpg'))
-      mimer.suggested_extension.should eql(".jpg")
+      expect(mimer.suggested_extension).to eq(".jpg")
     end
-    
+
     it "should work for gif" do
       mimer = Mimer.identify(fixture_path('google.gif'))
-      mimer.suggested_extension.should eql(".gif")
+      expect(mimer.suggested_extension).to eq(".gif")
     end
-    
+
     it "should work for png" do
       mimer = Mimer.identify(fixture_path('kirk.png'))
-      mimer.suggested_extension.should eql(".png")
+      expect(mimer.suggested_extension).to eq(".png")
     end
   end
-  
+
   describe "image" do
-    
+
     before(:each) do
       @mimer = Mimer.identify(fixture_path('facepalm.jpg'))
     end
-    
+
     it "should identify mime correctly" do
-      @mimer.mime_type.should match(/image\/jpeg/)
+      expect(@mimer.mime_type).to match(/image\/jpeg/)
     end
-    
+
     it "should find this image" do
-      @mimer.should be_image
+      expect(@mimer).to be_image
     end
-  end  
+  end
 end
